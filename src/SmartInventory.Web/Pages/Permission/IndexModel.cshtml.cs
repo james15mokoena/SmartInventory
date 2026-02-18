f73d3eda6c015935bf1a8cf57dbb95644dc49e0a
james15mokoena <jamesmokoena15@yahoo.com>
2026-02-18 06:48:03 +0200

    Implemented the Permission/Index page for permission management.

---
+++ src/SmartInventory.Web/Pages/Permission/IndexModel.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SmartInventory.Web.Pages.Permission;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ src/SmartInventory.Web/Pages/Sales/IndexModel.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SmartInventory.Web.Pages.Sales;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
