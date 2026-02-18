d99f319c2fedeb8d01a280d081a0e95a2b0b97ce
james15mokoena <jamesmokoena15@yahoo.com>
2026-02-18 07:12:29 +0200

    Implemented the Stock/Index page for stock management.

---
+++ src/SmartInventory.Web/Pages/Stock/IndexModel.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SmartInventory.Web.Pages.Stock;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
